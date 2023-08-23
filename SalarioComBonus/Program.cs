a656c59ee553de36c10572770b487d656ebd4cf7
Henrique Mendes <henriquemendes13@gmail.com>
2023-08-23 12:55:19 -0300

    Add project files.

---
+++ SalarioComBonus/Program.cs
@@ -0,0 +1,16 @@
+﻿//Faça um programa que leia o nome de um vendedor, o seu salário fixo e o
+//total de vendas efetuadas por ele no mês (em dinheiro).
+//Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas,
+//informar o total a receber no final do mês, com duas casas decimais.
+
+String nome;
+double salario, valorvendas, total;
+
+Console.WriteLine("DIGITE O NOME, SALARIO, E VALOR DAS VENDAS NO MÊS.");
+
+nome = (Console.ReadLine());
+salario = double.Parse(Console.ReadLine());
+valorvendas = double.Parse(Console.ReadLine());
+total = salario + valorvendas*0.15;
+
+Console.WriteLine("TOTAL: R$" + total.ToString("F2"));
